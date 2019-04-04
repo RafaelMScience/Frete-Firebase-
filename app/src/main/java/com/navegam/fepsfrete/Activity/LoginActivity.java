206227b460f28ee29a7a597363cc3074cbc256fa
Rafael <RafaelM.Science@gmail.com>
2019-04-04 15:05:00 -0400

    Init repo

---
+++ app/src/main/java/com/navegam/fepsfrete/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.navegam.fepsfrete.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.navegam.fepsfrete.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+    }
+}
